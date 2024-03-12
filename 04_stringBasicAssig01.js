8ca7e00b200fe135ad506716bc36edafdbd18b10
yogita-1710 <radheyavrushali2@gmail.com>
2024-03-12 10:16:30 +0530

    Task is use no argument, no return function in that  you have to use different string functions
    Task is use no argument, no return function in that  you have to use different string functions

---
+++ 04_stringBasicAssig01.js
@@ -0,0 +1,12 @@
+function stringBasics() {
+    console.log("========== Step 1 ==========");
+    console.log(`My dream company is"Tata Consultancy Services(TCS)"`);
+    console.log("========== Step 2 ==========");
+    var hobby1 = "Reading";
+    var hobby2 = "Gardening";
+    var hobby3 = "Traveling";
+    console.log(`My hobbies are:${hobby1},${hobby2},${hobby3}`);
+    var totalNoOfChar = hobby1.length + hobby2.length + hobby3.length;
+    console.log(`Sum of Total number of characters available in hobby1,hobby2,hobby3::${totalNoOfChar}`);
+}
+stringBasics();
